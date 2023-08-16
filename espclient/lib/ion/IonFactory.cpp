79cc0032cf25da1941ea80ebe833056d5f63ae58
ThingsPing <50649473+thingsping@users.noreply.github.com>
2023-08-16 21:34:44 +0530

    Update IonFactory.cpp
    Getting ready for new protocol implementation

--- espclient/lib/ion/IonFactory.cpp
+++ espclient/lib/ion/IonFactory.cpp
@@ -21,8 +21,6 @@ IonFactory :: IonFactory() {
     server = 0 ; 
     node = 0 ; 
     addrOnly= 0  ; 
-    things_created = new LinkedList<IonThing*>() ; 
-    names_created = new LinkedList<char*>(); 
     logger = QLogger :: getInstance() ; 
     init();
 }
@@ -45,56 +43,11 @@ IonFactory :: ~IonFactory() {
 }
 
 void IonFactory :: saveEntities() {
-    String json = "[" ;
-    int sz = things_created -> size(); 
-    for (int i = 0 ; i < sz ; i++)
-    {
-        IonThing* thing = things_created -> get(i);  
-        String name =  thing -> getName(); 
-        String location = thing -> getLocation() ; 
-        String tag = thing -> getTag() ; 
-        if (i != 0)
-            json += ",";
-        json.concat(F("{\"Name\":\"")); 
-        json.concat(name);
-        json.concat(F("\", \"Location\" : \"") );
-        json.concat(location); 
-        json.concat(F("\", \"Tag\":\""));
-    
-        json.concat(tag); 
-        json.concat(F("\"}")) ;  
-    }
-    json += "]";
-    configuration -> setConfiguration(json.c_str()) ; 
+    // To be implemented
 }
 
 void IonFactory :: init( ) {
-        configuration = IonConfig :: getConfiguration() ;
-        String srvaddr = configuration -> getServer() ; 
-        logger -> debug("Server returned by config = " + String(srvaddr)); 
-        int idx =  srvaddr.indexOf(":") ;  
-        if (idx != -1) {
-            if (addrOnly) {
-                free(addrOnly); 
-            }
-            addrOnly = (char*)(malloc(sizeof(char) * (idx+1))); 
-            strncpy(addrOnly, srvaddr.substring(0, idx).c_str(), idx ); 
-
-            addrOnly[idx] = 0 ; 
-            int port = 80;
-            String portStr = srvaddr.substring(idx+1); 
-            port = portStr.toInt(); // Validate - issue # 23
-            server = new IonServer(addrOnly, port); 
-         } else {
-            if (addrOnly) {
-                free(addrOnly); 
-            }
-            addrOnly = (char*)(malloc(sizeof(char) * srvaddr.length() +1)); 
-            strcpy(addrOnly, srvaddr.c_str()); 
-            server = new IonServer(addrOnly) ; 
-         }
-         node = new IonNode(configuration -> getDeviceId().c_str(), 
-                 configuration -> getDeviceKey().c_str());
+       //TBI
 }
 
 Sensor IonFactory :: createSensor(const char* name) {
@@ -114,66 +67,4 @@ Actuator IonFactory :: createActuator(String name, const char* action) {
     new_name[l] = 0; 
     names_created -> add(new_name); 
     return createActuator(new_name, action) ; 
-}
-
-Actuator IonFactory :: createActuator(const char* name, const char* action) {  
-    String loc =configuration -> getLocation(name);
-    if (loc.equals("")){
-        logger -> error("Location is blank! Has the entity been defined in the configuration?");
-    }
-    String tag = configuration -> getTag(name) ;  
-    Actuator * act = new Actuator(name, loc,  action);
-    act -> setTag(tag);
-    node -> addThing(act); 
-    things_created -> add(act);
-    return *act ; 
-}
-
-void IonFactory :: run(){    
-    if (configuration -> isConfigEmpty())
-        saveEntities() ; 
-    
-    BlinkType bt = NoBlink ; 
-    bool isconnected = server -> connectToWifi((configuration -> getSsid()).c_str(), 
-        (configuration -> getSsidPassword()).c_str()) ; 
-    if (!isconnected){    
-        bt = AirplaneBlink;
-    } else {
-        String a = server -> getLocalIP(); 
-        logger -> debug("Connected to wifi - ", server -> getLocalIP());             
-        // If we get out of the above call, it either means that the 
-        // server doesn't have a new config for this device or the 
-        // server doesn't even have a config for this device. 
-        if (configuration -> isConfigValid()){
-            node -> setAddress(server -> getLocalIP() ); 
-            server -> setNode(node); 
-            server -> registerWithServer();  
-        } else {        
-            bt = AirplaneBlink;
-        }
-        if (bt != NoBlink){
-            
-            while (true){
-                if (bt == AirplaneBlink){
-                    for (int i = 0 ; i < 5; i++){
-                        BoardSpecific :: setStatusLED(1); 
-                        delay(100); 
-                        BoardSpecific :: setStatusLED(0); 
-                        delay(100); 
-                    }
-                    BoardSpecific :: setStatusLED(1); 
-                    delay(500); 
-                    BoardSpecific :: setStatusLED(0); 
-                    delay(500); 
-                } else {
-                    BoardSpecific :: setStatusLED(1); 
-                    delay(500); 
-                    BoardSpecific :: setStatusLED(0); 
-                    delay(500); 
-                }
-            }
-        }
-
-    }
-    
 }
