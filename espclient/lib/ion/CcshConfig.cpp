3f2142b43d3fd774b05f12865d597c8223cd4902
ThingsPing <50649473+thingsping@users.noreply.github.com>
2023-08-16 21:32:37 +0530

    Update CcshConfig.cpp
    Starting working on supporting 1.0 protocol

--- espclient/lib/ion/CcshConfig.cpp
+++ espclient/lib/ion/CcshConfig.cpp
@@ -29,88 +29,11 @@ IonConfig::IonConfig(){
     configcontents = "" ; 
     m_cfgvalid = true ; // We'll start with the assumption that config is valid. If 
                 // endpoint chooses to set using JSON string, and if json is invalid, 
-                // then we'll set this to false there. 
-    things  = new LinkedList<ThingDetails*>();
+                // then we'll set this to false there.
 }
 
  void IonConfig :: convertJSONToConfig() {
-    DynamicJsonDocument ctlDoc(1024);
-    DeserializationError err = deserializeJson(ctlDoc, configcontents);
-     if (err) {
-        m_cfgvalid = false ; 
-        logger -> setLevel(QLogger :: LEVEL_DEBUG, true); // If the configuration is junk,
-            //it is  possible that the level has been set to a junk value which is 
-            // greater than DEBUG
-        logger -> debug("Error in configuration details", String(err.c_str()), 
-            "Contents = ", String(configcontents)); 
-    } else {
-        m_cfgvalid = true ; 
-        //deviceKey = ctlDoc["DeviceKey"].as<String>();
-        if (ctlDoc.containsKey("LogLevel")) {
-            loglevel = ctlDoc["LogLevel"].as<int>() ;
-        } else {
-            loglevel = QLogger ::LEVEL_INFO ; 
-        }
-        logger -> setLevel(loglevel); 
-        if (ctlDoc.containsKey("Server")) {
-            server = ctlDoc["Server"].as<String>();      
-        } else {
-            server = DEFAULTSERVER ; 
-        }
-        
-        if (ctlDoc.containsKey("PollingPort")) {
-            pollingPort = ctlDoc["PollingPort"]; 
-        }else {
-            pollingPort = DEFAULTPOLLINGPORT; 
-        }
-        if (ctlDoc.containsKey("IsLocalGateway")){
-            isLocalGw = ctlDoc["IsLocalGateway"]; 
-        } else {
-            isLocalGw = false ; 
-        }
-
-        if (ctlDoc.containsKey("IsRegistrationMandatory")){
-            m_regMandatory = ctlDoc["IsRegistrationMandatory"];
-        } else {
-            m_regMandatory = false ; 
-        }
-
-        JsonArray arr = ctlDoc["Things"].as<JsonArray>();
-
-        for (JsonObject repo : arr) { 
-            String rname, rloc, rTag; 
-            rname="";
-            rloc = DEFAULTLOCATION ; 
-            rTag = "" ; 
-            int paramCount = repo.size(); 
-            LinkedList<String>* pKeys = 0 ; 
-            LinkedList<String>* pVals = 0 ;
-            for (JsonPair kvp : repo){
-                const char* key = kvp.key().c_str() ; 
-                //logger -> debug("Key = " + String(key) + "; Val = " + repo[key].as<String>()); 
-                if (strcmp(key, "Name") == 0){
-                    rname = repo["Name"].as<String>();     
-                } else if (strcmp(key, "Location") == 0){
-                    rloc = repo["Location"].as<String>();   
-                } else if (strcmp(key, "Tag") == 0){
-                    rTag = repo["Tag"].as<String>();    
-                } else {
-                    if (pKeys == 0){
-                        pKeys = new LinkedList<String>() ; 
-                        pVals = new LinkedList<String>() ; 
-                    }
-                    pKeys -> add(String(key));
-                    pVals -> add(repo[key].as<String>());
-                    //logger -> debug ("Adding " + String(key) + "[" + repo[key].as<String>() + "]");
-                }
-            }
-            ThingDetails *td  = new ThingDetails(rname, rloc, rTag); 
-            td -> keys = pKeys; 
-            td -> vals = pVals ; 
-            things -> add(td); 
-        }
-        logger -> debug("Configuration initialized. Config = ", getConfigJson()); 
-    }
+    //TBD - have to get this working with new support for ION 
 }
 
 IonConfig* IonConfig :: getConfiguration() {
@@ -123,41 +46,20 @@ IonConfig* IonConfig :: getConfiguration() {
 void IonConfig :: setConfiguration(const char* thingsjson){
     // First check if the incoming contents is the same as the 
     // saved configuration :
-    String cc = getConfigJsonBegin(); 
+    String cc = getConfigJsonBegin();
     cc.concat(thingsjson); 
     cc.concat("}");     
 }
 
 String IonConfig :: getConfigJsonBegin(){
     String cc = "{"; 
-    cc.concat(F("\"LogLevel\" : ")); 
-    cc.concat(String(loglevel)) ; 
-    cc.concat(F(",  \"Server\":\"" )); 
-    cc.concat(server); 
-    cc.concat(F("\", \"IsLocalGateway\":")); 
-    cc.concat(isLocalGw); 
-    cc.concat(F(", \"Things\":")); 
+    
     return cc;
 }
 
 String IonConfig :: getConfigJson() {
     String cc = getConfigJsonBegin(); 
-    String thingsjson = "["; 
-    int sz = things -> size(); 
-    for (int i = 0 ; i < sz ; i++)
-    {
-        ThingDetails* thing = things -> get(i);  
-        if (i != 0)
-            thingsjson.concat(",") ;
-        thingsjson.concat(F("{\"Name\":\"")); 
-        thingsjson.concat(thing -> name);
-        thingsjson.concat(F("\", \"Location\" : \"") );
-        thingsjson.concat(thing -> location); 
-        thingsjson.concat(F("\", \"Tag\":\""));
-        thingsjson.concat(thing -> tag); 
-        thingsjson.concat(F("\"}")) ;  
-    }
-    thingsjson.concat("]");
+    String thingsjson = "[]"; 
     cc.concat(thingsjson); 
     cc.concat("}"); 
     return cc; 
@@ -185,30 +87,6 @@ int IonConfig :: getPollingPort() {
     return pollingPort ; 
 }
 
-String IonConfig :: getParamValue(const char* name, const char* key){
-    String paramValue = "" ; 
-    int sz = things -> size() ; 
-    String nmString = String(name); 
-    for (int i = 0 ; i < sz; i++) {
-       ThingDetails* td = things -> get(i); 
-       if (nmString.equals(td -> name)) {
-           LinkedList<String>* keys = td ->keys ; 
-           if (keys != 0 ) {
-             int szkeys = keys -> size() ; 
-             for (int j = 0 ; j < szkeys; j++){
-                 String curkey = keys -> get(j); 
-                 if (curkey.equals(key)){
-                     paramValue = td ->vals -> get(j); 
-                     break ; 
-                 }
-
-             }
-           }
-           break ; 
-       }
-    }
-    return paramValue ; 
-}
 
 String IonConfig :: getLocation (const char* name){
    String loc  = "" ; 
@@ -224,20 +102,6 @@ String IonConfig :: getLocation (const char* name){
    return loc;
 }
 
-String IonConfig :: getTag(const char* name){
-    String tag  = "" ; 
-    int sz = things -> size() ; 
-    String nmString = String(name); 
-    for (int i = 0 ; i < sz; i++) {
-        ThingDetails* td = things -> get(i); 
-        if (nmString.equals(td -> name)) {
-            tag = td -> tag ; 
-            break ; 
-        }
-    }
-    return tag.c_str();
-     
-}
 
 void IonConfig :: setSsidDetails(String ssid, String password){ 
     String ssidcontents = "{\"SSID\":\""; 
